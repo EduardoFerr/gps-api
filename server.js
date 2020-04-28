7e247a921444d0bcc0d24615e37b3ee523d9f0ee
EduardoFerr <ferr.tutorial@gmail.com>
2020-04-28 01:07:02 -0300

    vai vai vai

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-//require('dotenv').config()
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const db = require('./config/database')
 const routes = require('./routes')
 const PORT = process.env.PORT || 3001
+const cors = require('cors')
 
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', routes)
 
